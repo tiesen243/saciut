a2d5b450deaac374b354f6aaef02540187dac513
tiesen243 <ttien56906@gmail.com>
2025-08-25 07:12:36 +0700

    chore: minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import AppController from '@/app/app.controller'
 import AuthModule from '@/app/auth/auth.module'
 import PostModule from '@/app/post/post.module'
 import DrizzleService from '@/common/services/drizzle.service'
-import JwtService from '@/common/services/jwt.service'
 
 @Module({
   imports: [AuthModule, PostModule],
   controllers: [AppController],
-  providers: [DrizzleService, JwtService],
+  providers: [DrizzleService],
 })
 export default class AppModule {}
