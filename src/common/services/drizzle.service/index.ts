68cc66c5c46d7a0772c14961c4c971667ad3325f
tiesen243 <ttien56906@gmail.com>
2025-08-27 03:18:55 +0700

    fix: build

--- src/common/services/drizzle.service/index.ts
+++ src/common/services/drizzle.service/index.ts
@@ -7,7 +7,7 @@ import * as schema from './schema'
 
 @Injectable()
 export default class DrizzleService {
-  private connectionString = `postgresql://${env.DB_USER}:${encodeURIComponent(env.DB_PASSWORD)}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?schema=public`
+  private connectionString = `postgresql://${env.DB_USER}:${encodeURIComponent(env.DB_PASSWORD)}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}?schema=public&sslmode=require`
   private instance
 
   constructor() {
