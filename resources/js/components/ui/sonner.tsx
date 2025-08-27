bda731342438c3f7ef97e0d459522e72804d08d2
tiesen243 <ttien56906@gmail.com>
2025-08-27 22:05:23 +0700

    fix: build

--- resources/js/components/ui/sonner.tsx
+++ resources/js/components/ui/sonner.tsx
@@ -1,18 +1,20 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import type { ToasterProps } from 'sonner'
+import { Toaster as Sonner } from 'sonner'
+
+import { useTheme } from '@client/hooks/use-theme'
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = 'system' } = useTheme()
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
+          '--normal-bg': 'var(--popover)',
+          '--normal-text': 'var(--popover-foreground)',
+          '--normal-border': 'var(--border)',
         } as React.CSSProperties
       }
       {...props}
